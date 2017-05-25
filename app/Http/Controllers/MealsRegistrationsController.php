7c3ca01da630ff7c8d3c74c16e9427d44c919331
Jakob Buis <jakob@jakobbuis.nl>
2017-05-25 17:36:08 +0200

    scaffolded controllers and models

---
+++ app/Http/Controllers/MealsRegistrationsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Registration;
+use App\Meal;
+use Illuminate\Http\Request;
+
+class MealsRegistrationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Meal  $meal
+     * @param  \App\Registration  $registration
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Meal $meal, Registration $registration)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Meal  $meal
+     * @param  \App\Registration  $registration
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Meal $meal, Registration $registration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Meal  $meal
+     * @param  \App\Registration  $registration
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Meal $meal, Registration $registration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Meal  $meal
+     * @param  \App\Registration  $registration
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Meal $meal, Registration $registration)
+    {
+        //
+    }
+}
