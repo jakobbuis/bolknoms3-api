97d633a83195ee2ba02ca57603988f39ac2933c1
Jakob Buis <jakob@jakobbuis.nl>
2017-05-27 20:21:33 +0200

    fixed bug in routing that excluded the GET /meals route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::resource('meals', 'MealsController', ['except' => ['create', 'index']]);
+Route::resource('meals', 'MealsController', ['except' => ['create', 'edit']]);
 Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'update']]);
 Route::resource('/meals/{meal}/registrations', 'MealRegistrationsController');
