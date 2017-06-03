e65f7bd3a826326c7cb7877f8c7c1a3ea8a81462
Jakob Buis <jakob@jakobbuis.nl>
2017-06-03 22:42:35 +0200

    all models use soft deletes

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,8 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 abstract class Model extends \Illuminate\Database\Eloquent\Model
 {
+    /*
+     * No models are removed from the database upon deletion
+     */
+    use SoftDeletes;
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
     /**
      * The appropriate Fractal Transformer for this model
      * @return \League\Fractal\TransformerAbstract
