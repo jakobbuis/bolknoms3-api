ca416b3f75b909a3ba9939e8979a65be9ae1a00d
Jakob Buis <jakob@jakobbuis.nl>
2017-06-03 21:58:47 +0200

    add command to generate meals and set a daily schedule

---
+++ app/Console/Commands/GenerateMeals.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GenerateMeals extends Command
+{
+    protected $signature = 'bolknoms:meals:generate';
+    protected $description = 'Generate meals for the next week Monday through Thursday';
+
+    public function handle()
+    {
+        // Get next monday
+        $date = strtotime('next monday');
+
+        // Walk until the thursday (4 days)
+        for ($i=0; $i < 4; $i++) {
+            $current_date = date('Y-m-d', strtotime("+{$i} days", $date));
+            echo "Attempting {$current_date}...";
+            if (Meal::withTrashed()->whereRaw("DATE(meal_timestamp) = '$current_date'")->count() == 0) {
+                $meal = Meal::create([
+                    'meal_timestamp' => $current_date.' 18:30:00',
+                    'locked_timestamp' => $current_date.' 15:00:00',
+                ]);
+                Log::info("Generated meal for {$current_date}", $meal);
+            }
+            else {
+                Log::info("Skipped generating meal for {$current_date}, a meal exists for this date");
+            }
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\GenerateMeals::class,
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('bolknoms:meals:generate')->daily();
     }
 
     /**
