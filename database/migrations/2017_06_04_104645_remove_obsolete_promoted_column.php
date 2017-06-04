b81513528c2514f7da0e66c901317d7e12f50449
Jakob Buis <jakob@jakobbuis.nl>
2017-06-04 10:53:45 +0200

    removed obsolete meals.promoted column

---
+++ database/migrations/2017_06_04_104645_remove_obsolete_promoted_column.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveObsoletePromotedColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Move the data to the event column
+        DB::unprepared("UPDATE meals SET event = 'evenement' WHERE event IS NULL AND promoted = 1");
+
+        Schema::table('meals', function(Blueprint $table){
+            $table->dropColumn('promoted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meals', function(Blueprint $table){
+            $table->boolean('promoted');
+        });
+    }
+}
