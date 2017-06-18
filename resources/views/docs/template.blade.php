873a418f162a65a866e49a07136b3f1e4a8a3e68
Jakob Buis <jakob@jakobbuis.nl>
2017-06-18 14:30:08 +0200

    added some spacing above menu

--- resources/views/docs/template.blade.php
+++ resources/views/docs/template.blade.php
@@ -26,37 +26,35 @@
 
     <div class="container">
         <div class="columns">
-            <aside class="column is-3">
-                <nav class="menu">
-                    <ul class="menu-list">
-                        <li><a href="/docs/introduction">Introduction</a></li>
-                        <li><a href="/docs/working-with-oauth">Working with OAuth</a></li>
-                        <li><a href="/docs/authorisation">Authorisation</a></li>
-                        <li><a href="/docs/emails">E-mails</a></li>
-                    </ul>
+            <nav class="column is-3 menu">
+                <ul class="menu-list">
+                    <li><a href="/docs/introduction">Introduction</a></li>
+                    <li><a href="/docs/working-with-oauth">Working with OAuth</a></li>
+                    <li><a href="/docs/authorisation">Authorisation</a></li>
+                    <li><a href="/docs/emails">E-mails</a></li>
+                </ul>
 
-                    <p class="menu-label">
-                        Reference
-                    </p>
-                    <ul class="menu-list">
-                        <li><a href="/docs/meals">Meals</a></li>
-                        <li><a href="/docs/registrations">Registrations</a></li>
-                        <li><a href="/docs/users">Users</a></li>
-                    </ul>
+                <p class="menu-label">
+                    Reference
+                </p>
+                <ul class="menu-list">
+                    <li><a href="/docs/meals">Meals</a></li>
+                    <li><a href="/docs/registrations">Registrations</a></li>
+                    <li><a href="/docs/users">Users</a></li>
+                </ul>
 
-                    <p class="menu-label">
-                        Examples
-                    </p>
-                    <ul class="menu-list">
-                        <li><a href="/docs/see-all-open-meals">See all open meals</a></li>
-                        <li><a href="/docs/get-the-next-meal">Get the next meal</a></li>
-                        <li><a href="/docs/register-for-a-meal">Register for a meal</a></li>
-                    </ul>
-                </nav>
-            </aside>
+                <p class="menu-label">
+                    Examples
+                </p>
+                <ul class="menu-list">
+                    <li><a href="/docs/see-all-open-meals">See all open meals</a></li>
+                    <li><a href="/docs/get-the-next-meal">Get the next meal</a></li>
+                    <li><a href="/docs/register-for-a-meal">Register for a meal</a></li>
+                </ul>
+            </nav>
 
             <main class="column is-9">
-                <section class="section">
+                <section class="section content">
                     @yield('content')
                 </section>
             </main>
