19e622d1b4d3ed4a1bae876825b18e64736991e2
Jakob Buis <jakob@jakobbuis.nl>
2017-06-22 21:04:01 +0200

    added docs clarification on blocking users

--- resources/views/docs/pages/data-model.blade.php
+++ resources/views/docs/pages/data-model.blade.php
@@ -85,6 +85,12 @@
     <p>
         Members of De Bolk have a Bolkaccount with which they may register for meals. The User resource holds the Bolknoms-specific details for a user. When creating a registration for a user, the name and diet properties are copied over to the created registration.
     </p>
+    <p>
+        A board member can block a member from the system. A blocked member will not be able to register for any meal, though their existing registrations for upcoming and past meals are not removed. Blocks do not expire: they must be removed manually by a board member.
+    </p>
+    <p class="notification">
+        <strong>Lore:</strong> Blocking users is a feature that only exists because <a href="https://www.facebook.com/bdiepersloot">Boedi</a> requested for himself that he never be able to eat at De Bolk again. The feature is built solely for him and his user remains blocked to this day.
+    </p>
 
     <table class=table>
         <thead><tr>
