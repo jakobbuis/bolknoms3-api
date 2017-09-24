fba9616ef319b943b3c34c7225f87c222550589f
Jakob Buis <jakob@jakobbuis.nl>
2017-09-24 16:20:02 +0200

    remove obsolete output

--- app/Console/Commands/GenerateMeals.php
+++ app/Console/Commands/GenerateMeals.php
@@ -19,7 +19,6 @@ class GenerateMeals extends Command
         // Walk until the thursday (4 days)
         for ($i=0; $i < 4; $i++) {
             $current_date = date('Y-m-d', strtotime("+{$i} days", $date));
-            echo "Attempting {$current_date}...";
             if (Meal::withTrashed()->whereRaw("DATE(meal_timestamp) = '$current_date'")->count() == 0) {
                 $meal = Meal::create([
                     'meal_timestamp' => $current_date.' 18:30:00',
