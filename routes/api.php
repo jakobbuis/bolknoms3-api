96c92c3b1ba866eed88743f9c728aab16d45762b
Jakob Buis <jakob@jakobbuis.nl>
2017-09-24 15:55:58 +0200

    re-org api routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-// Custom routes for convenience
 Route::get('/users/blocked', 'UsersController@blocked');
 Route::get('/users/me', 'UsersController@me');
 Route::patch('/users/me', 'UsersController@updateMe');
+Route::resource('/users', 'UsersController', ['only' => ['index', 'show', 'update']]);
 
-// Basis resources
-Route::resource('meals', 'MealsController', ['except' => ['create', 'edit']]);
-Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'update']]);
+Route::resource('/meals', 'MealsController', ['except' => ['create', 'edit']]);
+
 Route::resource('/meals/{meal}/registrations', 'MealRegistrationsController');
-Route::resource('rules', 'RulesController', ['only' => ['index']]);
+
+Route::resource('/rules', 'RulesController', ['only' => ['index']]);
