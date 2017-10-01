d3d9baded3e5e71ea839650f94056a865e1685c0
Jakob Buis <jakob@jakobbuis.nl>
2017-10-01 20:20:17 +0200

    enable CORS headers

---
+++ config/cors.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedHeaders' => ['Content-Type'],
+    'allowedMethods' => ['GET', 'POST', 'PUT', 'PATCH', 'DETELE'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+];
